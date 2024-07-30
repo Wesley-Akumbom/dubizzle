e4a9575a61cb5b8938b2a2a6b768e902b53139f3
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-07-30 14:20:57 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:dubizzle/pages/chats.dart';
+import 'package:dubizzle/pages/favorites.dart';
+import 'package:dubizzle/pages/home.dart';
+import 'package:dubizzle/pages/menu.dart';
+import 'package:dubizzle/pages/place_an_ad.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +12,7 @@ void main() {
       '/home': (context) => Home(),
       '/favorites': (context) => Favorites(),
       '/place-an-ad': (context) => PlaceAnAd(),
-      '/chats': (context) => Menu(),
+      '/chats': (context) => Chats(),
       '/menu': (context) => Menu()
     },
   ));
