46e7f014ba487b9dd7293d4d81d615d1abe97569
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-07-31 10:04:20 +0100

    Update Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,12 +6,162 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                   const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    IconButton(
+                      icon: const Icon(Icons.notifications),
+                      onPressed: () {
+                        // Handle notification button press
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  children: const [
+                    CategoryCard('Property For Rent', Icons.home),
+                    CategoryCard('Property For Sale', Icons.sell),
+                    CategoryCard('Off-Plan Properties', Icons.architecture),
+                    CategoryCard('Rooms For Rent', Icons.hotel),
+                    CategoryCard('Motors', Icons.directions_car),
+                    CategoryCard('Jobs', Icons.work),
+                    CategoryCard('Classifieds', Icons.category),
+                    CategoryCard('Furniture & Garden', Icons.chair),
+                    CategoryCard('Community', Icons.people),
+                  ],
+                ),
+              ),
+              ListView(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: const [
+                  FeaturedListing('Featured Item 1'),
+                  FeaturedListing('Featured Item 2'),
+                  FeaturedListing('Featured Item 3'),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('Home Screen')
-      )
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add, color: Colors.redAccent, size: 55,),
+            label: 'Place Ad',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Menu',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.grey[900],
+        onTap: (index) {
+          // Handle navigation based on index
+        },
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String categoryName;
+  final IconData icon;
+
+  const CategoryCard(this.categoryName, this.icon, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 30, color: Colors.redAccent),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Text(
+              categoryName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[800],
+            ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FeaturedListing extends StatelessWidget {
+  final String title;
+
+  const FeaturedListing(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Text(title),
+        subtitle: const Text('Description of the featured item'),
+      ),
     );
   }
 }
