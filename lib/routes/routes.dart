e6636dca8b35e4764d300de0cd9cc4906daded2f
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-08-09 10:48:49 +0100

    Updates - Manage back button behaviour

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,12 @@
-import 'package:dubizzle/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dubizzle/screens/auth_screen.dart';
 import 'package:dubizzle/screens/chats_screen.dart';
 import 'package:dubizzle/screens/favorites_screen.dart';
 import 'package:dubizzle/screens/home_screen.dart';
 import 'package:dubizzle/screens/menu_screen.dart';
 import 'package:dubizzle/screens/place_ad_screen.dart';
 
-class AppRoutes{
+class AppRoutes {
   static const String auth = '/auth';
   static const String home = '/home';
   static const String favorites = '/favorites';
@@ -16,23 +16,48 @@ class AppRoutes{
 }
 
 class AppRouter {
-  static Route <dynamic> generateRoute(RouteSettings settings){
+  static Route generateRoute(RouteSettings settings) {
+    Widget screen;
+    bool canPop = false;
+
     switch (settings.name) {
       case AppRoutes.auth:
-        return MaterialPageRoute(builder: (_) => AuthScreen());
+        screen = AuthScreen();
+        // Auth screen can be popped (to close the app)
+        canPop = true;
       case AppRoutes.home:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+        screen = const HomeScreen();
+        // Home screen can be popped (to close the app)
+        canPop = true;
       case AppRoutes.favorites:
-        return MaterialPageRoute(builder: (_) => const FavoritesScreen());
+        screen = const FavoritesScreen();
       case AppRoutes.placeAd:
-        return MaterialPageRoute(builder: (_) => const PlaceAdScreen());
+        screen = const PlaceAdScreen();
       case AppRoutes.chats:
-        return MaterialPageRoute(builder: (_) => const ChatsScreen());
+        screen = const ChatsScreen();
       case AppRoutes.menu:
-        return MaterialPageRoute(builder: (_) => const MenuScreen());
+        screen = const MenuScreen();
       default:
-        return MaterialPageRoute(builder: (_) => AuthScreen());
-
+      // If an unknown route is requested, redirect to the home screen
+        screen = const HomeScreen();
+        canPop = true;
     }
+
+    return MaterialPageRoute(
+      builder: (_) => PopScope(
+        canPop: canPop,
+        onPopInvoked: (didPop) {
+          if (didPop) return;
+          if (canPop) {
+            // For AuthScreen and HomeScreen, this will allow the app to be closed
+            Navigator.of(_).pop();
+          } else {
+            // For other screens, navigate to the home screen
+            Navigator.of(_).pushReplacementNamed(AppRoutes.home);
+          }
+        },
+        child: screen,
+      ),
+    );
   }
 }
